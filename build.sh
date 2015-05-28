93c97f881b368b680b0cdd410877f784a50cf28c
Baptiste Mathus <batmat@batmat.net>
2015-05-28 22:48:28 +0200

    Ajouts --rm aux appels Docker

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 DIST=_dist
 mkdir -p $DIST
 echo "Prez generation"
-docker run -e PREZ=prez.adoc -v `pwd`:/documents batmat/asciidoctor-prez 
+docker run --rm -e PREZ=prez.adoc -v `pwd`:/documents batmat/asciidoctor-prez 
 mv -f prez.html $DIST
 
 echo "Lab generation"
-docker run -e PREZ=lab/lab.adoc -v `pwd`:/documents batmat/asciidoctor-prez 
-docker run -e PREZ=lab/index.adoc -v `pwd`:/documents batmat/asciidoctor-prez 
-docker run -e PREZ=lab/prerequisites.adoc -v `pwd`:/documents batmat/asciidoctor-prez 
+docker run --rm -e PREZ=lab/lab.adoc -v `pwd`:/documents batmat/asciidoctor-prez 
+docker run --rm -e PREZ=lab/index.adoc -v `pwd`:/documents batmat/asciidoctor-prez 
+docker run --rm -e PREZ=lab/prerequisites.adoc -v `pwd`:/documents batmat/asciidoctor-prez 
 mkdir -p $DIST/lab
 mv -f lab/*.html $DIST/lab
+cp -R ressources _dist
 
 echo "Generation ended. Please watch `pwd`/$DIST or browsing locally file:///`pwd`/$DIST/"
