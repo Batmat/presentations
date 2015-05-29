ee0d2ade78861ecdaa6a9c07052a43c9c9a9ed98
Baptiste Mathus <batmat@batmat.net>
2015-05-30 00:41:08 +0200

    Prez generation basics initialization

---
+++ build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -eou pipefail
+
+declare -A tipsMap
+
+TIPS_DIR=tips
+BUILD_OUTPUT_DIR=_dist
+PREZ_FILE=prez.adoc
+TIPS_OUTPUT=$BUILD_OUTPUT_DIR/tips.adoc
+mkdir -p $BUILD_OUTPUT_DIR
+
+TIP_COUNT=$( find $TIPS_DIR -name "*.adoc" -exec grep -e "^== " {} \; | wc -l )
+
+echo "Note: $TIP_COUNT total tips found"
+
+cp prez.adoc $BUILD_OUTPUT_DIR
+rm -f $TIPS_OUTPUT
+
+for dir in $( ls $TIPS_DIR/ )
+do
+	currentDir=$TIPS_DIR/$dir
+	echo "Processing $currentDir"
+	for adocFile in $( ls $currentDir/*.adoc )
+	do
+	   cat $adocFile | sed "s/\(^==.*\)$/\1 ({counter:tip}\/$TIP_COUNT)/g" >> $TIPS_OUTPUT
+	   echo >> $TIPS_OUTPUT
+	   echo >> $TIPS_OUTPUT
+	done
+done
+
+echo "Copying all the images of $TIPS_DIR"
+#TODO : check if no duplicate in naming (to prevent overwrite)
+
+cp -R resources $BUILD_OUTPUT_DIR
+
+find $TIPS_DIR -name "*.jpg" -or -name "*.png" -exec cp "{}" $BUILD_OUTPUT_DIR/resources \;
+
+cd $BUILD_OUTPUT_DIR
+echo "Prez generation"
+docker run --rm -e PREZ=prez.adoc -v `pwd`:/documents batmat/asciidoctor-prez 
+
+echo "Generation ended. Please watch `pwd`/prez.html or browse locally file:///`pwd`/prez.html"
