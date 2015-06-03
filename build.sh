c098a187f93b4bfd47c65550cfd3b8fd553f2cc1
Michael Pailloncy <mpapo.dev@gmail.com>
2015-06-03 06:50:22 +0200

    Fix html files generation   - Fix _dist cleanup (old files were not cleaned up)   - Loop to generate all lab HTML files instead of declaring each one

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 DIST=_dist
-mkdir -p $DIST
+rm -rf $DIST && mkdir -p $DIST
 echo "Prez generation"
 docker run --rm -e PREZ=prez.adoc -v `pwd`:/documents batmat/asciidoctor-prez 
 mv -f prez.html $DIST
 
 echo "Lab generation"
-docker run --rm -e PREZ=lab/lab.adoc -v `pwd`:/documents batmat/asciidoctor-prez 
-docker run --rm -e PREZ=lab/index.adoc -v `pwd`:/documents batmat/asciidoctor-prez 
-docker run --rm -e PREZ=lab/prerequisites.adoc -v `pwd`:/documents batmat/asciidoctor-prez 
+for lab in `ls lab/*.adoc`; do
+	echo "Generating html of $lab ..."
+	docker run --rm -e PREZ=$lab -v `pwd`:/documents batmat/asciidoctor-prez 
+done
 mkdir -p $DIST/lab
 mv -f lab/*.html $DIST/lab
 cp -R ressources _dist
