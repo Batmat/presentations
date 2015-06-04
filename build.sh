2a11720661818e376ce026cdb569afeecc0cb2ca
Baptiste Mathus <batmat@batmat.net>
2015-06-04 09:18:19 +0200

    lab -> labs rename

--- build.sh
+++ build.sh
@@ -4,13 +4,14 @@ echo "Prez generation"
 docker run --rm -e PREZ=prez.adoc -v `pwd`:/documents batmat/asciidoctor-prez 
 mv -f prez.html $DIST
 
-echo "Lab generation"
-for lab in `ls lab/*.adoc`; do
+echo "Labs generation"
+for lab in `ls labs/*.adoc`; do
 	echo "Generating html of $lab ..."
 	docker run --rm -e PREZ=$lab -v `pwd`:/documents batmat/asciidoctor-prez 
 done
-mkdir -p $DIST/lab
-mv -f lab/*.html $DIST/lab
+mkdir -p $DIST/labs
+mv -f labs/*.html $DIST/labs
+
 cp -R resources _dist
 
 echo "Generation ended. Please watch `pwd`/$DIST or browsing locally file:///`pwd`/$DIST/"
