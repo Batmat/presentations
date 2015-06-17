f1b89f7ffda1c0423228abb1585bc4a556ac6564
Michael Pailloncy <pailloncy.m@mipih.fr>
2015-06-17 10:37:54 +0200

    Generate labs with and without correction

--- build.sh
+++ build.sh
@@ -7,19 +7,26 @@ mv -f prez.html $DIST
 echo "Copying resources"
 time cp -R resources _dist
 
-echo "Labs generation"
-for lab in `ls labs/*.adoc`; do
+echo "Generating labs with correction"
+for file in labs/lab-correction*.adoc; do
+	labWithoutCorrection=${file/lab-correction/lab} 
+	cp $file $labWithoutCorrection
+	sed -r -i '/[\*]{4}/,/[\*]{4}/d' $labWithoutCorrection
+	sed -i 's/\.Correction$//g' $labWithoutCorrection
+done
+for lab in `ls labs/lab*.adoc`; do
 	echo "Generating html of $lab ..."
 	docker run --rm -e PREZ=$lab -v `pwd`:/documents batmat/asciidoctor-prez 
 done
 mkdir -p $DIST/labs
 mv -f labs/*.html $DIST/labs
 
+rm labs/lab-[0-9]-*
+
 echo "Manually change resources paths"
 cp -R .deck.js $DIST/resources/.deck.js
 sed -i 's/\.\.\/\.deck\.js/resources\/.deck.js/g' $DIST/prez.html
 sed -i 's/http:\/\/cdnjs\.cloudflare\.com\/ajax\/libs\/highlight\.js\/7\.3\/styles\/default\.min\.css/resources\/highlight.js.default.min.css/g' $DIST/prez.html
 sed -i 's/http:\/\/cdnjs\.cloudflare\.com\/ajax\/libs\/highlight\.js\/7\.3\/highlight\.min\.js/resources\/highlight.min.js/g' $DIST/prez.html
-
 
 echo "Generation ended. Please watch `pwd`/$DIST or browsing locally file:///`pwd`/$DIST/"
