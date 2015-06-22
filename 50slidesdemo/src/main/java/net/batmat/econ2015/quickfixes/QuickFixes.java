337afbcd4074534a3036818c63dd5f40c2a322b8
Baptiste Mathus <batmat@batmat.net>
2015-06-22 11:06:06 +0200

    Splitting sample code to ease finding them

--- 50slidesdemo/src/main/java/net/batmat/econ2015/quickfixes/QuickFixes.java
+++ 50slidesdemo/src/main/java/net/batmat/econ2015/quickfixes/QuickFixes.java
@@ -1,32 +1,22 @@
-package net.batmat.econ2015.other;
+package net.batmat.econ2015.quickfixes;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class SomeClassDifferentlyNamed {
+public class QuickFixes {
 	public static List<String> hop = Arrays.asList("a", "b", "c");
 
 	private String someAttribute;
 
-	public static void main(String[] args) {
-
-		hop.forEach(string -> {
-			System.out.println(string);
-		});
-
-		int i = 1;
+	public static void ternaryAndInvert(String[] args) {
 		String s = (hop != null) ? s = hop.get(0) : "somevalue";
 
-		String a = "boom";
 	}
 
-	private void anotherGreatMethodCastingObject() {
-		Object o = new Object();
+	private void unused() {
+		int i = 1;
 
-		if (o instanceof String) {
-
-		}
-		// here is some new code
+		String a = "boom";
 
 	}
 
